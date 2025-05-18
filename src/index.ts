a410f75c66ac33cd81095f1dc60d03daae403a86
Raimon <raimonrts@gmail.com>
2025-05-18 17:38:49 +0530

    eslint

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 window.onbeforeunload = function() {
   window.scrollTo(0, 0);
 };
@@ -18,6 +19,7 @@ document.addEventListener('DOMContentLoaded', () => {
   
   const loaderLottie = 'https://cdn.prod.website-files.com/65b94b2bab54c86c1cd618a8/682725b636da1042ae7797f8_H1_gradient.json';
   const heroLottie = 'https://cdn.prod.website-files.com/65b94b2bab54c86c1cd618a8/68272532ccd07135e49d65f4_H2.json';
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const sectionLottie='https://cdn.prod.website-files.com/65b94b2bab54c86c1cd618a8/6827264cc032f162afebc33f_Copy%20of%20h3______.json';
   
   const myAnimation = loadAndPlayLottie('lottie-loader', loaderLottie);
@@ -33,6 +35,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
         // Fade in the hero lottie at the same time as loader fade-out
         fadeIn('#hero-lottie', 0, 0, () => {
+           
           console.log('Hero lottie fade-in complete');
           
           // Load and play the hero animation
@@ -42,6 +45,7 @@ document.addEventListener('DOMContentLoaded', () => {
         // Simultaneously fade out the loader animation after a short delay
         setTimeout(() => {
           fadeOut('#lottie-loader', 0, 0, () => {
+             
             console.log('Loader fade-out complete');
             
             // Enable scrolling after the animation sequence is complete
